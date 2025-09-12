589de0c443e32b920b91fba95aa9b977791cbe01
TimoWeigold <116874314+TimoWeigold@users.noreply.github.com>
2025-09-12 10:47:34 +0200

    Add files via upload

---
+++ internal_reference_label/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "Internal Reference Label",
+    "version": "1.0",
+    "depends": ["stock"],
+    "author": "Copilot",
+    "category": "Inventory",
+    "description": "Print internal reference labels for serial numbers.",
+    "data": [
+        "report/internal_reference_label_report.xml",
+        "views/stock_production_lot_action.xml"
+    ],
+    "installable": True,
+    "application": False
+}
